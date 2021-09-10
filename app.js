b8e836b50472e52cfed299a6b9930717bcf1019a
dryptm <vinay9415756155@gmail.com>
2021-09-10 06:22:23 +0530

    asdasfadsf

--- app.js
+++ app.js
@@ -74,6 +74,6 @@ app.get("/mongo-video", function (req, res) {
 
 
 
-app.listen(8000, function () {
+app.listen(process.env.PORT || 8000, function () {
   console.log("Listening on port 8000!");
 });
